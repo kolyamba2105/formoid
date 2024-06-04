01ab5c2ed0cbe889a3975799b5b77fe7be2cf481
non25 <19non91@gmail.com>
2024-06-04 21:53:30 +0300

    Fix stale form values in setValues and reset (#5)
    * Fix stale form values in setValues and reset
    
    * Avoid needlessly recalculating initial formState on each render
    
    * Re-export UnknownRecord to fix CI

--- packages/formoid/src/useFieldArrayState.ts
+++ packages/formoid/src/useFieldArrayState.ts
@@ -15,7 +15,7 @@ import { UnknownRecord, forEach } from "./Record";
 
 export function useFieldArrayState<T extends UnknownRecord>(initialValues: Array<T>) {
   const persistentInitialValues = useRef(initialValues);
-  const [state, setState] = useState(persistentInitialValues.current.map(initializeForm));
+  const [state, setState] = useState(() => persistentInitialValues.current.map(initializeForm));
 
   const errors = useMemo(() => state.map(getErrors), [state]);
   const values = useMemo(() => state.map(getValues), [state]);
@@ -38,12 +38,9 @@ export function useFieldArrayState<T extends UnknownRecord>(initialValues: Array
   const remove = useCallback((index: number) => {
     setState(deleteAt(index));
   }, []);
-  const reset = useCallback(
-    (update?: Update<Array<T>>) => {
-      setState((update?.(values) ?? persistentInitialValues.current).map(initializeForm));
-    },
-    [values],
-  );
+  const reset = useCallback((update?: Update<Array<T>>) => {
+    setState((state) => (update?.(state.map(getValues)) ?? persistentInitialValues.current).map(initializeForm));
+  }, []);
   const setErrors: SetFieldArrayErrors<T> = useCallback((index, key, errors) => {
     setState(modifyAt(index, (group) => formStateManager(group).setErrors(key, errors)));
   }, []);
